df7c4a843c0dec94fe2cdebf11bb34a00c10deac
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-02 14:53:49 -0600

    created enpoints for movies, movies by id, login, logout, and ratings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import session from "express-session";
 import morgan from "morgan";
 import ViteExpress from "vite-express";
-import { Movie, Rating, User } from "./src/model.js";
+import { User, Rating, Movie, db } from "./src/model.js";
 
 const app = express();
 const port = "8000";
@@ -22,8 +22,46 @@ app.get("/api/movies", async (req, res) => {
 
 app.get("/api/movies/:movieId", async (req, res) => {
   const { movieId } = req.params;
-  const movie = await Movie.findByPK(movieId);
+  const movie = await Movie.findByPk(movieId);
   res.json(movie);
+});
+
+app.post("/api/auth", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: { email: email },
+  });
+  if (user && user.password === password) {
+    req.session.userId = user.userId;
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+app.post("/api/logout", async (req, res) => {
+  if (!req.session.userId) {
+    res.status(401).json({ error: "Unauthorized" });
+  } else {
+    req.session.destroy();
+    res.json({ success: true });
+  }
+});
+
+app.get("/api/ratings", async (req, res) => {
+  const { userId } = req.session;
+  if (!userId) {
+    res.status(401).json({ error: "Unauthorized" });
+  } else {
+    const user = await User.findByPk(userId);
+    const ratings = await user.getRatings({
+      include: {
+        model: Movie,
+        attributes: ["title"],
+      },
+    });
+    res.json(ratings);
+  }
 });
 
 ViteExpress.listen(app, port, () =>
